cfc4cdf155aea8188734991176afc56674ca8bbe
jian <janine.jian.chen@gmail.com>
2019-05-09 09:38:02 +0800

    解决跨域问题

--- src/main/java/com/boot/security/server/SecurityApplication.java
+++ src/main/java/com/boot/security/server/SecurityApplication.java
@@ -2,6 +2,10 @@ package com.boot.security.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * 启动类
@@ -15,4 +19,14 @@ public class SecurityApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
 	}
+	
+	@Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
